b0514e96875a098963d73b0d0b44060a668b416b
jR4dh3y <radheykalra901@gmail.com>
2023-10-02 18:14:52 +0530

    smooking zaza again cos i added goofy aa js hehehehhehehe

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get all the item title links and their descriptions
+const itemTitles = document.querySelectorAll('.item-title');
+const itemDescriptions = document.querySelectorAll('.item-description');
+
+// Add a click event listener to each item title
+itemTitles.forEach((title, index) => {
+    title.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the link from navigating
+        
+        // Toggle the visibility of the corresponding item description
+        if (itemDescriptions[index].style.display === 'block') {
+            itemDescriptions[index].style.display = 'none';
+        } else {
+            itemDescriptions[index].style.display = 'block';
+        }
+    });
+});
