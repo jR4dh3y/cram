deed6ba0a7507dd72cd2f9aa4f8cfe347a8781b2
Radhey Kalra <radheykalra901@gmail.com>
2023-10-03 22:59:35 +0530

    zazazazazaz     redid    everything

--- script.js
+++ script.js
@@ -2,16 +2,54 @@
 const itemTitles = document.querySelectorAll('.item-title');
 const itemDescriptions = document.querySelectorAll('.item-description');
 
-// Add a click event listener to each item title
-itemTitles.forEach((title, index) => {
-    title.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent the link from navigating
-        
-        // Toggle the visibility of the corresponding item description with animation
-        if (itemDescriptions[index].style.display === 'block') {
-            itemDescriptions[index].style.display = 'none';
+// Get the filter and sort select elements
+const filterSelect = document.getElementById('filter-select');
+const sortSelect = document.getElementById('sort-select');
+
+// Add event listeners to filter and sort the items
+filterSelect.addEventListener('change', filterItems);
+sortSelect.addEventListener('change', sortItems);
+
+// Function to filter items by category
+function filterItems() {
+    const selectedCategory = filterSelect.value;
+
+    itemTitles.forEach((title, index) => {
+        const category = title.closest('.category').querySelector('h2').textContent;
+        const item = title.closest('.item');
+
+        if (selectedCategory === 'All' || selectedCategory === category) {
+            item.style.display = 'block';
         } else {
-            itemDescriptions[index].style.display = 'block';
+            item.style.display = 'none';
         }
     });
-});
+}
+
+// Function to sort items by price
+function sortItems() {
+    const selectedSort = sortSelect.value;
+
+    const itemsArray = Array.from(document.querySelectorAll('.item'));
+    const sortedItems = itemsArray.sort((a, b) => {
+        const priceA = parseFloat(a.querySelector('p:last-child').textContent.replace('Price: $', ''));
+        const priceB = parseFloat(b.querySelector('p:last-child').textContent.replace('Price: $', ''));
+
+        if (selectedSort === 'LowToHigh') {
+            return priceA - priceB;
+        } else if (selectedSort === 'HighToLow') {
+            return priceB - priceA;
+        }
+    });
+
+    const menu = document.querySelector('.menu');
+    menu.innerHTML = '';
+
+    sortedItems.forEach((item) => {
+        menu.appendChild(item);
+    });
+}
+
+// Initially, filter items by 'All' categories and sort by 'LowToHigh' price
+filterItems();
+sortItems();
